a72327f03195838d6b8dc40a5250c28a98d83910
Joshua <23joshtc@gmail.com>
2023-08-21 20:19:31 -0400

    added delete route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -39,12 +39,31 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedCategory = await Category.update(req.body, {
+      where: {
+        id:req.params.id,
+      }
+    });
+    res.status(200).json(updatedCategory);
+    } catch (err) {
+      res.status(400).json(err);
+    }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedCategory = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(deletedCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+  
 });
 
 module.exports = router;
